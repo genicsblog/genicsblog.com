e4a767e87b94019fbac43df360d1ef36390a3fa5
Gourav Khunger <gouravkhunger18@gmail.com>
2021-12-09 00:28:39 +0530

    Add tailwindcss and remove default theme

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [
+        './_includes/**/*.html',
+        './_layouts/**/*.html',
+        './*.html',
+    ],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
