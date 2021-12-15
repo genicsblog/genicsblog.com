db5ebac63ed748baa223a65f20132d0dfa9d087c
gouravkhunger <gouravkhunger18@gmail.com>
2021-12-15 13:08:26 +0000

    Update gh-pages to output generated at c4e331d

---
+++ assets/js/theme.js
@@ -0,0 +1,1 @@
+function toggleNav(){var e=document.getElementById("menu-open"),d=document.getElementById("menu-close");d.classList.contains("hidden")?(e.classList.add("hidden"),d.classList.remove("hidden")):(e.classList.remove("hidden"),d.classList.add("hidden"))}
