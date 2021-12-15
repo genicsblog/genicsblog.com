d35cd07dabda9aab23bfc1b39a39cf663951f81d
Gourav Khunger <gouravkhunger18@gmail.com>
2021-12-15 22:59:40 +0530

    Completed navigation bar

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,17 @@
 function toggleNav() {
-    var menuOpen = document.getElementById("menu-open")
-    var menuClose = document.getElementById("menu-close")
+    let menuOpen = document.getElementById("menu-open")
+    let menuClose = document.getElementById("menu-close")
+    let mobileNav = document.getElementById("mobile-nav")
     
     if (menuClose.classList.contains("hidden")) {
+        // nav is opened
         menuOpen.classList.add("hidden")
         menuClose.classList.remove("hidden")
+        mobileNav.classList.remove("hidden")
     } else {
+        // nav is closed
         menuOpen.classList.remove("hidden")
         menuClose.classList.add("hidden")
+        mobileNav.classList.add("hidden")
     }
 }
