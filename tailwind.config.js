c12e7b70fc0fe2b1ffe3d4588c5053c5b8600a35
Gourav Khunger <gouravkhunger18@gmail.com>
2021-12-22 21:44:37 +0530

    More updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 module.exports = {
+    important: true,
     content: [
         './_includes/**/*.html',
         './_layouts/**/*.html',
         './*.html',
+    ],
+    safelist: [
+        'bg-android',
+        'bg-web',
+        'hover:border-android',
+        'hover:border-web'
     ],
     theme: {
         extend: {
             colors: {
                 'background': '#16161f',
                 'primary': '#2564eb',
+                'web': '#F06529',
                 'android': '#3ddc84'
             }
         },
