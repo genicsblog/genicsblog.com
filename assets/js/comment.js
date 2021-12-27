4b20bdb541832e88f26b551a1221067de755eecb
Gourav Khunger <gouravkhunger18@gmail.com>
2021-12-27 20:50:21 +0530

    More updates

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -10,7 +10,7 @@ var addComment = function () {
         return document.getElementById(id);
     };
 
-    document.getElementById('modal').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <p class="mb-0"> </p>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    document.getElementById('modal').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog"> <div class="modal-dialog shadow" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal"> &times; </button> </div> <div class="modal-body"> <p class="mb-0"> </p>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
     // $("#modal").hide();
 
     var submitButton = select(".submit-form");
